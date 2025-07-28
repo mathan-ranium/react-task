205a8a0831096a6576e6565029901ced9a6162bf
mathan-ranium <mathan.kumar@ranium.in>
2025-07-28 10:39:19 +0530

    changes

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -7,6 +7,7 @@ const authSlice = createSlice({
     token: null,
     loading: false,
     error: null,
+    user: null
   },
   reducers: {
     logout: (state) => {
@@ -23,6 +24,7 @@ const authSlice = createSlice({
       .addCase(loginUser.fulfilled, (state, action) => {
         state.loading = false;
         state.token = action.payload.token;
+        state.user = action.payload.user;
         localStorage.setItem('token', action.payload.token);
       })
       .addCase(loginUser.rejected, (state, action) => {
